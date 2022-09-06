abf7d18c113c30adc4514c9cf79d7735a1ebce4e
sebastianbarreram <sbarrera.96@hotmail.com>
2022-09-06 15:26:52 -0500

    Person list form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'listado';
+  title = 'Listado de Personas';
+  personas: Persona[] = [
+    new Persona('Sebastian', 'Barrera'),
+    new Persona('Diana', 'Marín'),
+    new Persona('Humberto', 'Barrera'),
+  ];
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  agregarPersona(): void {
+    if (this.nombreInput != '' && this.apellidoInput != '')
+      this.personas.push(new Persona(this.nombreInput, this.apellidoInput));
+  }
 }
