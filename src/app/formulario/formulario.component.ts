987ca147258f2901b78164a3022d8136aeed6f0f
sebastianbarreram <sbarrera.96@hotmail.com>
2022-09-06 16:37:24 -0500

    Local reference in angular

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -7,16 +7,18 @@ import { Persona } from '../persona.model';
   styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent {
-  nombreInput: string = '';
-  apellidoInput: string = '';
+  // nombreInput: string = '';
+  // apellidoInput: string = '';
 
   @Output() personaCreada = new EventEmitter<Persona>();
 
-  agregarPersona(): void {
-    if (this.nombreInput != '' && this.apellidoInput != '')
-      //this.personas.push(new Persona(this.nombreInput, this.apellidoInput));
+  agregarPersona(
+    nombreInput: HTMLInputElement,
+    apellidoInput: HTMLInputElement
+  ): void {
+    if (nombreInput.value != '' && apellidoInput.value != '')
       this.personaCreada.emit(
-        new Persona(this.nombreInput, this.apellidoInput)
+        new Persona(nombreInput.value, apellidoInput.value)
       );
   }
 }
