5a1b18f9c6f6f40e6f6a1c90aae04ef16a422199
sebastianbarreram <sbarrera.96@hotmail.com>
2022-09-07 15:54:02 -0500

    Child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,14 @@ import { PersonasComponent } from './personas/personas.component';
 
 const routes: Routes = [
   { path: '', component: PersonasComponent },
-  { path: 'personas', component: PersonasComponent },
-  { path: 'personas/agregar', component: FormularioComponent },
-  { path: 'personas/:id', component: FormularioComponent },
+  {
+    path: 'personas',
+    component: PersonasComponent,
+    children: [
+      { path: 'agregar', component: FormularioComponent },
+      { path: ':id', component: FormularioComponent },
+    ],
+  },
 ];
 
 @NgModule({
